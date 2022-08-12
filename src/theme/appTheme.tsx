cf96697d6ad7b618944e5a5b8f7a298676642c15
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-08-11 23:35:05 -0400

    add base of the app

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: '#000'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 20,
+    }
+})
